8e01f22e241aebc88c5f9884a157dbdcb9770fff
facu8990 <facu.vdp@gmail.com>
2022-06-13 21:35:41 +0000

    nextcloud install prep

--- install/install-nextcloud.sh
+++ install/install-nextcloud.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-echo "Hola Nextcloud"
+
+echo "Copiando archivos base para Contenedor Nextcloud y OpenOffice"
+
+echo "Creando directorio /opt/nextcloud..."
+[[ ! -d /opt/nextcloud ]] && sudo mkdir /opt/nextcloud
+[[ -d /opt/nextcloud ]] && echo "Directorio Creado"
+
+echo "Creando link ~/git/compose/nextcloud-compose.yml /opt/nextcloud/docker-compose.yml..."
+[[ ! -L /opt/nextcloud/docker-compose.yml ]] && sudo ln -s ../compose/npm-compose.yml /opt/nextcloud/docker-compose.yml
+[[ -L /opt/nextcloud/docker-compose.yml ]] && echo "Link a Compose establecido"
