fd1c36aa8d62b4d3d84eccb588c49f3e98432add
facu8990 <facu.vdp@gmail.com>
2022-06-13 21:19:20 +0000

    npm install v1

--- install/install-npm.sh
+++ install/install-npm.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+echo "Copiando archivos base para Contenedor NPM"
+
 echo "Creando directorio /opt/npm..."
-sudo mkdir /opt/npm
+[[ ! -d /opt/npm ]] && sudo mkdir /opt/npm
+[[ -d /opt/npm ]] && echo "Directorio Creado"
 
 echo "Creando link ~/git/compose/npm-compose.yml /opt/npm/docker-compose.yml..."
-ln -s ../compose/npm-compose.yml /opt/npm/docker-compose.yml
-
+[[ ! -L /opt/npm/docker-compose.yml ]] && sudo ln -s ../compose/npm-compose.yml /opt/npm/docker-compose.yml
+[[ -L /opt/npm/docker-compose.yml ]] && echo "Link a Compose establecido"
