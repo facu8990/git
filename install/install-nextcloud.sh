0778fe55fa7789b1bd30f18af85f970e32ace576
facu8990 <facu.vdp@gmail.com>
2022-06-13 21:36:46 +0000

    nextcloud install

--- install/install-nextcloud.sh
+++ install/install-nextcloud.sh
@@ -7,5 +7,5 @@ echo "Creando directorio /opt/nextcloud..."
 [[ -d /opt/nextcloud ]] && echo "Directorio Creado"
 
 echo "Creando link ~/git/compose/nextcloud-compose.yml /opt/nextcloud/docker-compose.yml..."
-[[ ! -L /opt/nextcloud/docker-compose.yml ]] && sudo ln -s ../compose/npm-compose.yml /opt/nextcloud/docker-compose.yml
+[[ ! -L /opt/nextcloud/docker-compose.yml ]] && sudo ln -s ../compose/nextcloud-compose.yml /opt/nextcloud/docker-compose.yml
 [[ -L /opt/nextcloud/docker-compose.yml ]] && echo "Link a Compose establecido"
