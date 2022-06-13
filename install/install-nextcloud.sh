21e1aae9242326d6f6d7a74ab986867d8d0d3b38
facu8990 <facu.vdp@gmail.com>
2022-06-13 21:49:33 +0000

    nextcloud install v1 & npm install touchup

--- install/install-nextcloud.sh
+++ install/install-nextcloud.sh
@@ -2,10 +2,23 @@
 
 echo "Copiando archivos base para Contenedor Nextcloud y OpenOffice"
 
-echo "Creando directorio /opt/nextcloud..."
 [[ ! -d /opt/nextcloud ]] && sudo mkdir /opt/nextcloud
-[[ -d /opt/nextcloud ]] && echo "Directorio Creado"
+[[ -d /opt/nextcloud ]] && echo "Directorio /opt/nextcloud Creado"
 
-echo "Creando link ~/git/compose/nextcloud-compose.yml /opt/nextcloud/docker-compose.yml..."
+[[ ! -d /opt/nextcloud/db ]] && sudo mkdir /opt/nextcloud/db
+[[ -d /opt/nextcloud/db ]] && echo "Directorio /opt/nextcloud/db Creado"
+
+[[ ! -d /opt/nextcloud/config ]] && sudo mkdir /opt/nextcloud/config
+[[ -d /opt/nextcloud/config ]] && echo "Directorio /opt/nextcloud/config Creado"
+
+[[ ! -d /storage/nextcloud/data ]] && sudo mkdir -p /storage/nextcloud/data
+[[ -d /storage/nextcloud/data ]] && echo "Directorio /storage/nextcloud/data Creado"
+
+[[ ! -d /opt/nextcloud/onlyoffice/data ]] && sudo mkdir -p /opt/nextcloud/onlyoffice/data
+[[ -d /opt/nextcloud/onlyoffice/data ]] && echo "Directorio /opt/nextcloud/onlyoffice/data Creado"
+
+[[ ! -d /opt/nextcloud/onlyoffice/logs ]] && sudo mkdir -p /opt/nextcloud/onlyoffice/logs
+[[ -d /opt/nextcloud/onlyoffice/logs ]] && echo "Directorio /opt/nextcloud/onlyoffice/logs Creado"
+
 [[ ! -L /opt/nextcloud/docker-compose.yml ]] && sudo ln -s ../compose/nextcloud-compose.yml /opt/nextcloud/docker-compose.yml
-[[ -L /opt/nextcloud/docker-compose.yml ]] && echo "Link a Compose establecido"
+[[ -L /opt/nextcloud/docker-compose.yml ]] && echo "Link a ~/git/compose/nextcloud-compose.yml establecido"
--- install/install-npm.sh
+++ install/install-npm.sh
@@ -2,10 +2,8 @@
 
 echo "Copiando archivos base para Contenedor NPM"
 
-echo "Creando directorio /opt/npm..."
 [[ ! -d /opt/npm ]] && sudo mkdir /opt/npm
-[[ -d /opt/npm ]] && echo "Directorio Creado"
+[[ -d /opt/npm ]] && echo "Directorio /opt/npm Creado"
 
-echo "Creando link ~/git/compose/npm-compose.yml /opt/npm/docker-compose.yml..."
 [[ ! -L /opt/npm/docker-compose.yml ]] && sudo ln -s ../compose/npm-compose.yml /opt/npm/docker-compose.yml
-[[ -L /opt/npm/docker-compose.yml ]] && echo "Link a Compose establecido"
+[[ -L /opt/npm/docker-compose.yml ]] && echo "Link a ~/git/compose/npm-compose.yml establecido"
