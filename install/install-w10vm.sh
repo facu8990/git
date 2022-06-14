19d704e8154ebf091356aec2ab8d564a4a9c3eb6
facu8990 <facu.vdp@gmail.com>
2022-06-14 05:03:55 +0000

    w10vm stonks

--- install/install-w10vm.sh
+++ install/install-w10vm.sh
@@ -3,18 +3,18 @@
 [[ ! -d /opt/win10vm ]] && sudo mkdir /opt/win10vm
 [[ -d /opt/win10vm ]] && echo "Directorio /opt/win10vm Listo"
 
-[[ ! -f /opt/win10vm/disk.hd.img.raw ]] && sudo qemu-img create /opt/win10vm/disk.hd.img.raw 40G
-[[ -f /opt/win10vm/disk.hd.img.raw ]] && echo "Disco Virtual ../win10vm/disk.hd.img.raw Listo"
+[[ ! -f /opt/win10vm/disk.qcow2 ]] && sudo qemu-img create -f qcow2 /opt/win10vm/disk.qcow2 40G
+[[ -f /opt/win10vm/disk.qcow2 ]] && echo "Disco Virtual /opt/win10vm/disk.qcow2 Listo"
 
-
-sudo qemu-img create /opt/win10vm/disk.hd.img.raw 40G
-
-sudo qemu-system-x86_64 -bios /usr/share/ovmf/OVMF.fd -enable-kvm -cpu host -smp 4 -m 4096 -net user  -net nic,model=virtio -vnc -vga qxl -cdrom ~/images/win10.iso -drive file=/opt/win10vm/disk.hd.img.raw,format=raw,if=virtio -drive file=~/images/win10.iso,index=1,media=cdrom
-
-qemu-system-x86_64
- -enable-kvm
- -m 8G
- -cpu host,kvm=off
- -cdrom ~/Win10_1511_English_x64.iso
- -drive file=/usr/share/ovmf/ovmf_x64.bin,format=raw,if=pflash,readonly
- -drive file=/mnt/WindowsOS/WindowsOS,format=raw
+sudo qemu-system-x86_64 \
+ -name win10vm \
+ -bios /usr/share/ovmf/OVMF.fd \
+ -enable-kvm \
+ -m 4096 \
+ -net user  \
+ -nic user,hostfwd=tcp::5555-:3389,model=virtio-net-pci \
+ -nographic \
+ -vnc :0 \
+ -drive file=/opt/win10vm/disk.qcow2,index=0,media=disk,if=virtio \
+ -drive file=~/images/win10.iso,index=2,media=cdrom \
+ -drive file=~/images/virtio-win-0.1.217.iso,index=3,media=cdrom
