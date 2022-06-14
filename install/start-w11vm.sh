ced9b4e7f5520370659938beb20dc5c44e5713d2
facu8990 <facu.vdp@gmail.com>
2022-06-14 06:27:36 +0000

    w11vm v1

--- install/start-w11vm.sh
+++ install/start-w11vm.sh
@@ -4,17 +4,14 @@
 
 [[ -f /opt/win10vm/disk.qcow2 ]] && \
 sudo qemu-system-x86_64 \
- -name win10vm \
- -bios /usr/share/ovmf/OVMF.fd \
- -enable-kvm \
- -m 4096 \
- -usb -device usb-tablet \
- -nographic \
- -vnc :0 \
- -net user  \
- -nic user,hostfwd=tcp::5555-:3389,model=virtio-net-pci \
- -drive file=/opt/win10vm/disk.qcow2,index=0,media=disk,if=virtio \
- -net user  \
- -nic user,hostfwd=tcp::5555-:3389,model=virtio-net-pci
+-name win10vm \
+-bios /usr/share/ovmf/OVMF.fd \
+-enable-kvm \
+-smp 2 \
+-m 4096 \
+-usb -device usb-tablet \
+-nographic \
+-nic user,hostfwd=tcp::5555-:3389 \
+-drive file=/opt/win10vm/disk.qcow2,index=0,media=disk,if=virtio
 
 [[ ! -f /opt/win10vm/disk.qcow2 ]] && echo "Disco Virtual /opt/win10vm/disk.qcow2 Ausente"
